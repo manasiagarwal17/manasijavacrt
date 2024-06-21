dd54f4528c9ddc1ffb27805cb397a055f52843d3
Manasi Agarwal <159446748+manasiagarwal17@users.noreply.github.com>
2024-06-21 09:20:33 +0530

    Add files via upload

---
+++ sqroot.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class sqroot
+{
+public static void main(String args[])
+{
+Scanner s=new Scanner(System.in);
+int a;
+a=s.nextInt();
+double b=Math.sqrt(a);
+System.out.println(b);
+}
+}
