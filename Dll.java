bc26a42eb85f86e1a160b6542f052e2d44173217
Manasi Agarwal <159446748+manasiagarwal17@users.noreply.github.com>
2024-09-13 10:34:37 +0530

    Add files via upload

---
+++ Dll.java
@@ -0,0 +1,104 @@
+/* double linked list*/ 
+import java.util.*;
+class Node
+{
+	int data;
+	Node prev,next;
+	
+	Node(int x)
+	{
+	   data=x;
+           prev=null;
+	   next=null;
+	}
+}
+class DoubleList
+{
+	Node head,tail;
+	DoubleList()
+	{
+	   head=tail=null;
+	}
+	void create(int x)
+	{
+	   Node n=new Node(x);
+	   if(head==null)
+	   head=tail=n;
+	   else
+	   {
+		tail.next=n;
+		n.prev=tail;
+		tail=n;
+	   }
+	}
+	void rshow()
+	{
+	   if(head==null)
+	   {
+		System.out.println("list is empty");
+		return;
+	   }
+	   Node temp=head;
+	   while(temp!=null)
+	   {
+		System.out.print(temp.data+" ");
+	        temp=temp.next;
+	   }
+	}
+	void lshow()
+	{
+	   if(head==null)
+	   {
+		System.out.println("list is empty");
+		return;
+	   }
+	   Node temp=tail;
+	   while(temp!=null)
+	   {
+		System.out.print(temp.data+" ");
+	        temp=temp.prev;
+	   }
+	}
+}
+class Dll
+{
+	public static void main(String args[])
+	{
+	   Scanner s=new Scanner(System.in);
+	   DoubleList d1=new DoubleList();
+	   for(;;)
+	   {
+		System.out.println();
+		System.out.println("1.Create\n2.Print\n3.Exit");
+		System.out.println("enter your choice");
+		int ch=s.nextInt();
+	        if(ch==1)
+		{
+		   System.out.println("enter an element");
+		   int x=s.nextInt();
+		   d1.create(x);
+	   	}
+	  	else if(ch==2)
+	   	{
+		   System.out.println("which way to traverse\n1.Forward\n2.Backward");
+		   int r=s.nextInt();
+		   if(r==1)
+			d1.rshow();
+		   else if(r==2)
+			d1.lshow();
+		   else
+			System.out.println("invalid choice");
+		}
+		else if(ch==3)
+			break;
+		else
+			System.out.println("wrong choice");
+	   }
+	}
+}		
+
+	   
+
+		
+	  
+   	
